7d1ed70c017050d496d777a76360313e3cec9872
Psycho0verload <mail@psycho0verload.de>
2017-12-27 01:04:18 +0100

    Update scanspecifictag.sh

--- script/scanspecifictag.sh
+++ script/scanspecifictag.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+filename=/tmp/bluetooth_devices.$$
+hcitool lescan > $filename & sleep 10
+pkill --signal SIGINT hcitool
+sleep 1
+searchresult=$(grep -c $1 $filename)
+if [ $searchresult -gt 0 ]; then
+        echo 1
+else
+        echo 0
+fi
+rm $filename

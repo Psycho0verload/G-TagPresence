f2aa298c8ba3968d25b47d22fee093f9ec3dac8f
Psycho0verload <mail@psycho0verload.de>
2018-01-04 16:09:09 +0100

    Update cronjob.sh
    Fix Bug

--- cronjob.sh
+++ cronjob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/usr/bin/php scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_1
-/usr/bin/php scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_2
-/usr/bin/php scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_3
-/usr/bin/php scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_4
-/usr/bin/php scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_5
+/usr/bin/php /var/www/html/presence/scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_1
+/usr/bin/php /var/www/html/presence/scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_2
+/usr/bin/php /var/www/html/presence/scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_3
+/usr/bin/php /var/www/html/presence/scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_4
+/usr/bin/php /var/www/html/presence/scanspecifictag.php tagMac=7C:2F:80:CE:EF:44 item=PresenceGtag_5
